975644ddecdf1a31a976393678af3b937c14a042
abc873693 <abc873693@gmail.com>
2016-11-09 22:27:22 +0800

    更新索引

--- index.php
+++ index.php
@@ -1,27 +1,23 @@
 <html>
 <?php
-$i = $_GET["count"]==''?0:$_GET["count"];
-$method = $_GET["method"];
-$myfile = fopen("think.txt", "r") or die("Unable to open file!");
-// Output one line until end-of-file
-$s = array();
-$x = 0;
-while(!feof($myfile)) {
-	$s[$x] = fgets($myfile); 
-	$x++;
-  
+$dir = "/etc/php/";
+
+// 判斷是否為目錄
+if(is_dir($dir)){
+
+if ($dh = opendir($dir)) {
+while (($file = readdir($dh)) !== false) {
+
+//只過讀取出php 的檔案
+if (strpos( $file2, '.php')){
+echo "filename: $file : filetype: " . filetype($dir . $file) . "\n";
 }
-if($method=="up")$i = ($i==0?0:$i-1);
-else if($method=="rand")$i = rand(0,$x-1);
-else if($method=="next")$i = ($i==$x-1?$x-1:$i+1);
-else ;
-echo "靜思語<br>";
-echo "第".($i+1)."筆,共".($x)."筆<br>";
-echo $s[$i]."<br>";
-echo "<button type=button formmethod=get onClick=location.href='think.php?count=$i&method=up'>上一個</button>";
-echo "<button type=button formmethod=get onClick=location.href='think.php?count=$i&method=rand'>隨機</button>";
-echo "<button type=button formmethod=get onClick=location.href='think.php?count=$i&method=next'>下一個</button>";
-fclose($myfile);
+
+}
+closedir($dh);
+}
+
+}
 ?>
 
 </html>
